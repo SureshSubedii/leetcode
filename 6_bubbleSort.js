fdde53d22fd803b4d2be2ef6920670874e975d76
SureshSubedii <sureshsubedi485@gmail.com>
2023-07-13 13:52:36 +0545

    Bubble sort

---
+++ 6_bubbleSort.js
@@ -0,0 +1,13 @@
+let arr = [20,10, 1, 2, 11, 9, 8, 19, 16];
+
+for (let i = 0; i <= arr.length ; i++) {
+  for (let j = i+1; j < arr.length; j++) {
+    if (arr[i] > arr[j]) {
+      let temp = arr[i];
+      arr[i] = arr[j ];
+      arr[j] = temp;
+    }
+  }
+}
+
+console.log(arr);
