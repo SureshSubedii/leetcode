1da630aee26833ee6d15c60b6f70626808f5ceb6
SureshSubedii <sureshsubedi485@gmail.com>
2023-07-14 14:30:52 +0545

    Longest CommonPrefix

---
+++ 8.js
@@ -0,0 +1,22 @@
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return '';
+    }
+
+    let commonPrefix = strs[0];
+    // console.log(strs[0].indexOf(commonPrefix))
+    
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(commonPrefix) !== 0) {
+            commonPrefix = commonPrefix.slice(0, commonPrefix.length - 1);
+            
+            if (commonPrefix === '') {
+                return '';
+            }
+        }
+    }
+
+    return commonPrefix;
+};
+
+console.log(longestCommonPrefix(['flower', 'flour', 'flw'])); 
