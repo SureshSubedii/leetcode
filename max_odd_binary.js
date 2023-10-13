2141edb50babc0fd95a30d40ae0d18211a5e45c0
SureshSubedii <sureshsubedi485@gmail.com>
2023-10-13 17:43:40 +0545

    Returning max odd binary number

---
+++ max_odd_binary.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var maximumOddBinaryNumber = function (s) {
+  let count = 0;
+  let length = s.length;
+  let first = -1;
+  let arr = [...s];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "1") {
+      count ++;
+      arr[i] = "0" 
+    }
+  }
+  if ( length === count){
+    return s
+  }
+  for (let i = 0; i < count; i++) {
+    if (i  === 0 ) {
+      length --
+      arr[length ] = "1";
+    } else {
+        first ++
+      arr[first ] = "1";
+    }
+  }
+  return arr.join("");
+};
+console.log(maximumOddBinaryNumber("0111"));
