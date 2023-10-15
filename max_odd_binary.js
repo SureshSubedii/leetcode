cc769159f92dc5a37e2dd62b8bbd4d4768d57c26
SureshSubedii <sureshsubedi485@gmail.com>
2023-10-15 23:06:28 +0545

    Minimum cost stairs

--- max_odd_binary.js
+++ max_odd_binary.js
@@ -9,22 +9,22 @@ var maximumOddBinaryNumber = function (s) {
   let arr = [...s];
   for (let i = 0; i < s.length; i++) {
     if (s[i] === "1") {
-      count ++;
-      arr[i] = "0" 
+      count++;
+      arr[i] = "0";
     }
   }
-  if ( length === count){
-    return s
+  if (length === count) {
+    return s;
   }
   for (let i = 0; i < count; i++) {
-    if (i  === 0 ) {
-      length --
-      arr[length ] = "1";
+    if (i === 0) {
+      length--;
+      arr[length] = "1";
     } else {
-        first ++
-      arr[first ] = "1";
+      first++;
+      arr[first] = "1";
     }
   }
   return arr.join("");
 };
-console.log(maximumOddBinaryNumber("0111"));
+console.log(maximumOddBinaryNumber("100101"));
